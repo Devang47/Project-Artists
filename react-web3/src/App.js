2c7512bdf5cb640ee7649129b2156e275bb5784d
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-10-18 09:25:38 +0530

    metamask added

--- react-web3/src/App.js
+++ react-web3/src/App.js
@@ -57,7 +57,15 @@ function App() {
   const [name, setName] = useState("");
   const [input, setInput] = useState("");
 
-  useEffect(() => getName);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    getName();
+    if (typeof window.ethereum !== "undefined") {
+      console.log("MetaMask is installed!");
+    }
+  });
 
   async function getName() {
     const username = await contract.methods.name().call();
@@ -73,23 +81,54 @@ function App() {
     getName();
   };
 
+  const login = async () => {
+    try {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      setCurrentUser(accounts[0]);
+      setLoggedIn(true);
+      console.log(accounts);
+    } catch (err) {
+      throw new Error("error");
+    }
+  };
+
   return (
     <div className="">
-      <form action="#" onSubmit={changeName} className="p-20 text-lg mx-auto block">
-        <input
-          type="text"
-          className="py-2 px-4 bg-gray-100 focus:bg-gray-200 outline-none border-0 "
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button className="bg-red-600 text-white py-2 px-4 font-bold hover:scale-105 transform duration-75">
-          Change name
+      {!loggedIn ? (
+        <button
+          className=" cursor-pointer bg-blue-500 text-white font-bold py-2 px-4 shadow rounded"
+          onClick={login}
+        >
+          Login with Metamask
         </button>
-      </form>
+      ) : (
+        <>
+          <form action="#" onSubmit={changeName} className="p-10 text-lg mx-auto block">
+            <input
+              type="text"
+              className="py-2 px-4 bg-gray-100 focus:bg-gray-200 outline-none border-0 "
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className="bg-red-600 text-white py-2 px-4 font-bold hover:scale-105 transform duration-75">
+              Change name
+            </button>
+          </form>
 
-      <div className="bg-gray-100 border-2 border-dashed py-5 px-10 text-xl mx-auto w-10/12">
-        {name}
-      </div>
+          <div className="m-10 py-2 px-4 bg-gray-100 w-min mr-auto whitespace-nowrap">
+            Your address:{" "}
+            <code className="border-2 border-gray-300 ml-3 bg-gray-200 py-1 px-2">
+              {" "}
+              {currentUser || "not defined"}
+            </code>
+          </div>
+
+          <div className="bg-gray-100 border-2 border-dashed py-5 px-10 text-xl w-min whitespace-nowrap m-10">
+            name:{" "}
+            <code className="border-2 border-gray-300 ml-3 bg-gray-200 py-1 px-2">{name}</code>
+          </div>
+        </>
+      )}
     </div>
   );
 }
